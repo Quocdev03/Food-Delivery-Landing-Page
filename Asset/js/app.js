76b7f698061f42f1964468cf5a6848e252570c77
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-01-18 00:51:56 +0700

    complete page

--- Asset/js/app.js
+++ Asset/js/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
-    $('.food-list').slick(
-        {
-            infinite: false, // true: lặp lại, false: không lặp lại
-            slidesToShow: 4, // số lượng ảnh hiển thị
-            slidesToScroll: 1, // số lượng ảnh cuộn
-            prevArrow: // nút prev
-                `<button type='button' class='slick-prev pull-left slick-arrow'><ion-icon name="chevron-back-outline"></ion-icon></button>`,
-            nextArrow: // nút next
-                `<button type='button' class='slick-next pull-right slick-arrow'><ion-icon name="chevron-forward-outline"></ion-icon></button>`,
-        }
-    );
+   $('.food-list').slick(
+      {
+         infinite: false, // true: lặp lại, false: không lặp lại
+         slidesToShow: 4, // số lượng ảnh hiển thị
+         slidesToScroll: 1, // số lượng ảnh cuộn
+         prevArrow: // nút prev
+            `<button type='button' class='slick-prev pull-left slick-arrow'><ion-icon name="chevron-back-outline"></ion-icon></button>`,
+         nextArrow: // nút next
+            `<button type='button' class='slick-next pull-right slick-arrow'><ion-icon name="chevron-forward-outline"></ion-icon></button>`,
+      }
+   );
 });
