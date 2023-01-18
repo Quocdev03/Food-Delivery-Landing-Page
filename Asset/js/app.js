310b5fc4fd2b3be4583c31cfc28a3bb6a0852099
Cao Chi Quoc <ccquoc2100346@student.ctuet.edu.vn>
2023-01-18 15:37:11 +0700

    update menu toggle

--- Asset/js/app.js
+++ Asset/js/app.js
@@ -1,3 +1,4 @@
+// Slick slider 
 $(document).ready(function () {
    $('.food-list').slick(
       {
@@ -10,4 +11,30 @@ $(document).ready(function () {
             `<button type='button' class='slick-next pull-right slick-arrow'><ion-icon name="chevron-forward-outline"></ion-icon></button>`,
       }
    );
+});
+
+// Menu toggle
+// Sự kiện domcontentloaded dùng để chạy code ngây cả khi chưa load hết html
+document.addEventListener("DOMContentLoaded", function () {
+   // Tạo biến lưu trữ các thẻ html cần sử dụng
+   const menuOpen = document.querySelector(".menu-toggle");
+   const menuClose = document.querySelector(".navigation-close");
+   const menu = document.querySelector(".navigation");
+   // Nếu không tìm thấy thẻ html trên thì return
+   if (!menuOpen || !menuClose || !menu) return;
+   // Thêm class active khi click vào nút toggle
+   menuOpen.addEventListener("click", function () {
+      menu.classList.add("active");
+   });
+   // Xoá class active khi click vào nút close
+   menuClose.addEventListener("click", function () {
+      menu.classList.remove("active");
+   });
+   // Xoá class active khi click cả bên ngoài menu
+   document.addEventListener("click", function (e) {
+      // Nếu click vào menu hoặc nút toggle thì xoá class active
+      if (!menu.contains(e.target) && !e.target.matches(".menu-toggle")) {
+         menu.classList.remove("active");
+      }
+   });
 });
